cd4d5e5fcbe37efa3d82cb7d959d3e5b3e3247cc
Enrique <Enrique_ceas@hotmail.com>
2020-06-03 23:30:56 -0600

    Initial Commit

---
+++ SodaU/SodaU/LogIn.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SodaU
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogIn : ContentPage
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+        private async void MenuSemanal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MenuSemanal());
+        }
+    }
+}
---
+++ SodaU/SodaU/MenuSemanal.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SodaU
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuSemanal : ContentPage
+    {
+        public MenuSemanal()
+        {
+            InitializeComponent();
+        }
+        private async void Desayuno_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MenuDesayuno());
+        }
+        private async void Almuerzo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MenuAlmuerzos());
+        }
+        private async void Cena_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MenuCena());
+        }
+    }
+}
